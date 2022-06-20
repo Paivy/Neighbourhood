1784d147d5dfa5a7101225ff65f41b8b574c1560
Paivy <paivyeshirera@gmail.com>
2022-06-20 11:17:03 +0300

    registered models in the admin.py

--- neighapp/admin.py
+++ neighapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . models import *
 # Register your models here.
+
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Contacts)
+admin.site.register(Business)
