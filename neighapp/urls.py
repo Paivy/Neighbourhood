cc8eed1780d188915c39381d8620d7b956a35552
Paivy <paivyeshirera@gmail.com>
2022-06-21 05:26:14 +0300

    added he profile urls and singlepost urls and deleted the all business and all posts urls

--- neighapp/urls.py
+++ neighapp/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
     path('',views.home, name="home"),
+    re_path('hood/(?P<location>\w+)',views.single_hood, name="single_hood"),
+    re_path('profile/(?P<username>\w+)',views.profile, name="profile"),
 ]
